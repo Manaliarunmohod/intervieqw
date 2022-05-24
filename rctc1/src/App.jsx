5310c6d139537255c3a33052b9fbcb53421a7148
Manali mohod <masaimanali@gmail.com>
2022-05-24 17:48:57 +0530

    changes

--- rctc1/src/App.jsx
+++ rctc1/src/App.jsx
@@ -3,13 +3,37 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
+
+
+
+ 
+  
+
+ 
+
+
+
 function App() {
   const [data,setData]=useState([])
-
+  const [btn, setBtn] = useState(false);
   useEffect(()=>{
     GetData()
   },[])
-
+  const handleOrder=()=>{
+    const data_new = [...data];
+    if(btn === false){
+      data_new.sort(function(a, b){
+        return a.salary-b.salary;
+      })
+      setBtn(true);
+    } else if(btn === true){
+      data_new.sort(function(a, b){
+        return b.salary-a.salary;
+      })
+      setBtn(false);
+    } 
+    setData(data_new);
+    }
 
   const GetData=()=>{
 
@@ -31,8 +55,19 @@ function App() {
 
 
     <div className="App">
-     
-       
+     <div style={{marginBottom:"40px"}}>
+{data.map((el,id)=>{
+  return(
+    <div key={id}>
+<div>{el.name}</div>
+<div>{el.salary}</div>
+    </div>
+  )
+})}
+     </div>
+
+
+    <button onClick={handleOrder}>{btn===false? "Accending" : "Decending"}</button>
     </div>
   )
 }
